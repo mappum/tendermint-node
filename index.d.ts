a00de5597c463f06b7ff9fa7568ee37abda8fdc2
Yaram <pootisspencerherenao@gmail.com>
2018-12-09 18:02:41 +1100

    Add Typescript type definitions

---
+++ index.d.ts
@@ -0,0 +1,12 @@
+import { ChildProcess } from 'child_process';
+
+export function init(path: string): Promise<null>
+export function initSync(path: string)
+
+export function node(path: string, opts: object): ChildProcess
+
+export function lite(target: string, path: string, opts: object): ChildProcess
+
+export function version(): string
+
+export function genValidator(): string
